3f2ae42c7998330f04bb70df2779b42e74c7f725
Gustavo Vilas Boas <gustavo.boas@startaideia.com.br>
2021-07-01 22:54:58 -0300

    refact: remove unused interfaces

--- app/Contracts/Services/WebService.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace App\Contracts\Services;
-
-interface WebService
-{}
--- app/Drivers/Email/GmailDriver.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Drivers;
-
-use App\Contracts\Drivers\EmailDriver;
-
-class GmailDriver implements EmailDriver
-{}
